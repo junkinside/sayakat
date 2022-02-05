fa948a6cccbf1b11bca6fe40da0cedbc97005a41
erkin312 <erkin.kk@gmail.com>
2022-02-05 13:08:36 +0600

    Displaying cards on the map

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -11,7 +11,7 @@ import {
 } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
-import { Rating } from '@material-ui/lab/Rating';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
@@ -32,6 +32,12 @@ const PlaceDetails = ({ place }) => {
         <Typography gutterBottom variant="h5">
           {place.name}
         </Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Rating value={Number(place.rating)} readOnly />
+          <Typography gutterBottom variant="subtitle1">
+            Out of {place.num_reviews} reviews
+          </Typography>
+        </Box>
         <Box display="flex" justifyContent="space-between">
           <Typography variant="subtitle1">Price</Typography>
           <Typography gutterBottom variant="subtitle1">
