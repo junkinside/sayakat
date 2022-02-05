640d77e9f53047c44f0aad63896af07a205f2295
erkin312 <erkin.kk@gmail.com>
2022-02-05 15:22:52 +0600

    Implemented scrolling and filtering

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,25 @@
 import axios from 'axios';
 
-const URL =
-  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude:sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-      },
-      headers: {
-        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-        'x-rapidapi-key': '4f5f485a55msh310c24f6e546ed2p151712jsn8ba9bc73e724',
-      },
-    });
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+          'x-rapidapi-key':
+            '4f5f485a55msh310c24f6e546ed2p151712jsn8ba9bc73e724',
+        },
+      }
+    );
     return data;
   } catch (error) {
     console.log(error);
