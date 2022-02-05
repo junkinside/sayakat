663000795f6eed38e10eef6ce763bc88da0365e1
erkin312 <erkin.kk@gmail.com>
2022-02-05 16:47:24 +0600

    Implemented searching

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search'
+import SearchIcon from '@material-ui/icons/Search';
 
-import useStyles from './styles'
+import useStyles from './styles';
 
+const Header = ({ setCoordinates }) => {
+  const classes = useStyles();
 
-const Header = () => {
-  const classes = useStyles()
+  const [autocomplete, setAutocomplete] = useState(null);
+
+  const onLoad = (autoC) => {
+    setAutocomplete(autoC);
+  };
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+    setCoordinates({ lat, lng });
+  };
+
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
@@ -18,14 +30,17 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore New Places
           </Typography>
-          {/* <Autocomplete> */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
-              <div className={classes.SearchIcon}>
+              <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
-              <InputBase placeholder='Search...' classes={{root: classes.inputRoot, input: classes.inputInput}}/>
+              <InputBase
+                placeholder="Search..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
             </div>
-          {/* </Autocomplete> */}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
